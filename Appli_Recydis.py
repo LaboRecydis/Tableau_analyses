58bd6db0683e90cb9873135b4b057cfee4c86fdc
LaboRecydis <122175049+LaboRecydis@users.noreply.github.com>
2023-01-07 20:08:48 +0100

    Update Appli_Recydis.py

--- Appli_Recydis.py
+++ Appli_Recydis.py
@@ -33,7 +33,7 @@ if __name__=="__main__":
   
 
     # Display the LOGO
-    img1 = Image.open("IMG_PAPREC.png")
+    img1 = Image.open("IMG_PAPREC.jpg")
     img2 = Image.open("IMG_RECYDIS.png") 
     st.sidebar.image(img1, width=300)
     st.sidebar.image(img2, width=300)
